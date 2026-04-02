1cc59e6fa2928e2a8451ae854cdd09c2a3769628
kartikkoli <kartikkoli230@gmail.com>
2026-04-02 18:33:40 +0530

    print element of arr

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int arry [5];
+    printf("enter the five numbers ");
+    for( int i=0;i<5;i++){
+        scanf("%d",&arry[i]);
+        printf(" you entered :");
+        
+        } 
+        for( int i=1;i<=5;i++){
+            printf("%d ",arry[i]);}
+        
+    
+    return 0;
+}
