d1754d0ab216329d96ce1e7bb19ea7862c4e5851
kartikkoli <kartikkoli230@gmail.com>
2026-04-03 18:16:03 +0530

    print 2D arry

--- array/2darray.c
+++ array/2darray.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 int main(){
-    printf("learn 2d array lerran today.");
+    int arry[3][3]={
+    {78,45,18},         //0
+    {45,18,33},         //1
+    {45,23,45}          //2
+    };
+// printf("element of arry %d",arry[0][2]);
+for(int row=0;row<3;row++){
+    for( int col=0;col<3;col++){
+        printf("%d ",arry[row][col]);
+    }
+    printf("\n");
+}
+
 
 }
+// dry run
+// row=0
+//     col=0
+//         arry[0][0]=78
+//     col=1
+//         arry[0][1]=45
+//     col=2
+//         arry[0][2]=18
+// row=1
+//     col=0
+//         arry[1][0]=45
+//     col=1
+//         arry[1][1]=18
+//     col=2
+//         arry[1][2]=33
